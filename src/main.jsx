b66c21e663d83fa2ab7bb9a291b1301d07879177
Prince-mmpp <princerajput4537@gmail.com>
2026-03-14 15:03:50 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard'
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const router = createBrowserRouter([
+  {
+    element:<App />,
+    children:[
+      {
+        path:'/dashboard',
+        element:<Dashboard />
+      }
+    ]
+  },
+  {
+    path:'/',
+    element:<Home />
+  },
+  
+  {
+    path:'/auth/sign-in',
+    element:<SignInPage />
+  }
+])
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router = {router} />
+    </ClerkProvider>
+  </StrictMode>
 )
